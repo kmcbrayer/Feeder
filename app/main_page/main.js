72e23af8f0f7340d081169cb7aefb45e07991f81
Kevin McBrayer <kmcbrayer77@gmail.com>
2014-11-13 16:45:48 -0500

    styling and starting swiping fxns

--- app/main_page/main.js
+++ app/main_page/main.js
@@ -10,12 +10,15 @@ angular.module('feederApp')
     $q.all([
       $http.get('/api/twitter/statuses').success(function(data) {
         twitterList = data;
+        $scope.twitterList = twitterList;
       }),
       $http.get('/api/instagram/feed').success(function(feed) {
         instagramList = feed;
+        $scope.instagramList = instagramList;
       }),
       $http.get('/api/youtube/subscriptions').success(function(subs) {
         youtubeList = subs;
+        $scope.youtubeList = youtubeList;
       })
     ]).then(function() {
       if (twitterList !== []) {
